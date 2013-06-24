85828972fe966ec9a405ebbe7cba9288b661fe97
Daniel Amitay <hello@danielamitay.com>
2013-06-24 14:55:27 -0400

    Remove unnecessary code/comments

--- DAReloadActivityExample/DAReloadActivityExample/ViewController.m
+++ DAReloadActivityExample/DAReloadActivityExample/ViewController.m
@@ -13,18 +13,11 @@
 @synthesize navigationBarItem;
 @synthesize viewButton;
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
-}
-
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor scrollViewTexturedBackgroundColor];
     
     navigationBarItem = [[DAReloadActivityButton alloc] init];
@@ -62,36 +55,8 @@
     }
 }
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
